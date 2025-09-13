9a4bfc008773be0e217d9fd0bad2d290b4a5dcd2
DeTosis <92999941+DeTosis@users.noreply.github.com>
2025-09-13 23:37:11 +0500

    sb_take | sb_at, documentation

--- sb.c
+++ sb.c
@@ -3,23 +3,21 @@
 
 int main(void)
 {
-    /*
-        SB Initialisation
-    */
     string_builder sb = {0};
-    // sb_new(string_builder*);
+    // void sb_new(string_builder* sb);
     sb_new(&sb);
-    /*
-        Library Usage:
-    */
-    // sb_append(string_builder*, const char*) dynamicly expands sb to fit provided string
-    sb_append(&sb, "Hello World");
 
-    printf("[%s]", sb.data);
+    // void sb_append(string_builder* sb, const char* value);
+    sb_append(&sb, "Hello World\n");
+    printf("%s", sb.data); //Hello World\n
 
-    /*
-        You are responsible for freeng sb, so do not forget it
-    */
+    //char sb_at(string_builder* sb, size_t pos)
+    printf("%c\n", sb_at(&sb, 2)); //l
+
+    // const char* sb_take(string_builder* sb, size_t at, size_t count)
+    printf("%s\n", sb_take(&sb, 6, 5)); //World
+
+    // void sb_free(string_builder* sb)
     sb_free(&sb);
     return 0;
 }
--- sb.h
+++ sb.h
@@ -19,22 +19,109 @@ typedef struct
     size_t size;
 } string_builder;
 
+
+/*
+    ********************************
+          SB CORE MANIPULATIONS
+    ********************************
+*/
+/*
+    desc:
+        allocates new string_builder dynamicly with base size
+        of 'MINIMAL_SIZE'
+        
+    input:
+        string_builder* sb - reference to a tring_builder
+
+    NOTES:
+        * Allocation of sb is contiguous so it is better to not use 
+        this library for satrings larger then ~ 300mb
+
+        * If sb is already initialised all data will be lost 
+        when second initialization would take place, data is
+        cleared at the initialisation state;
+*/
 void sb_new(string_builder* sb);
+/*
+    desc:
+        free the data of the string_builder's buffer
+        essentialy clearing it.
+
+    input:
+        string_builder* sb - reference to a tring_builder
+
+    NOTES:
+        * You are responsible for freeng string_builder
+        Usage after free is only single-thread safe. 
+ 
+        * If you want only to clear string_builder, better to call
+        sb_new(string_builder*), because data would be freed and
+        reallocated with 'MINIMAL_SIZE', it is irrelevent to create a
+        new object or manually free string_builder.
+*/
 void sb_free(string_builder* sb);
 
+/*
+    ********************************
+        SB CONTENT MANIPULATIONS
+    ********************************
+*/
+/*
+    desc:
+        appends string_builder's buffer at the end with cstring provided. 
+        buffer is expanded dynamicly with a 'SCALE_FACTOR'
+
+    input:
+        string_builder* sb - reference to a tring_builder
+
+    NOTES: 
+        * makes a copy for a stored string_builder string and a copy
+        for `const char*` string provided, so it is quite inefficient
+        on a large text scale, can result in cash losses, memory allocation failures
+        or memory fragmentation, be aware.
+*/
 void sb_append(string_builder* sb, const char* value);
+/*
+    ********************************
+        SB CONTENT ACCESSING
+    ********************************
+*/
+/*
+    desc:
+        safely returns a character from a string_builder's buffer
+
+    input: 
+        string_builder* sb - reference to a tring_builder
+        size_t pos - pos of a character that should be returned
+
+    return:
+        character, in case of an error `underflow | overflow` returns '\0'
+*/
+char sb_at(string_builder* sb, size_t pos);
+/*
+    desc:
+        safely takes a part of a string_buiilder's buffer
+    
+    input: 
+        string_builder* sb - reference to a tring_builder
+        size_t at - start point in the string_builder's buffer
+        size_t count - length of the string to take  
+
+    return:
+        cstyle string, in case of an error 
+        `underflow | overflow | bad allocation etc..` 
+        returns "" - emty string 
+*/
+const char* sb_take(string_builder* sb, size_t at, size_t count);
 
 // *** SB_IMPLEMENTATION ***
 #ifdef SB_IMPLEMENTATION
 
 void sb_new(string_builder* sb)
 {
-    if (sb->data || sb->size != 0)
-    {
-        free(sb->data);
-        sb->data = NULL;
-        sb->size = 0;
-    }
+    free(sb->data);
+    sb->data = NULL;
+    sb->size = 0;
 
     sb->data = malloc(MINIMAL_SIZE);
     if (!sb->data)
@@ -76,8 +163,31 @@ void sb_append(string_builder* sb, const char* value)
     sb->data[clen + len] = '\0';
 }
 
-#endif //SB_IMPLEMENTATION
+char sb_at(string_builder* sb, size_t pos)
+{
+    if (sb->size < pos || sb->size == 0) return '\0';
+    return sb->data[pos];
+}
+const char* sb_take(string_builder* sb, size_t at, size_t count)
+{
+    if (sb_at(sb, at + count) == '\0') return "";
 
+    char* result = malloc(count);
+    if (!result)
+    {
+        printf("[ ERROR ] 'sb_take' 'result' allocation failed");
+        return "";
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        result[i] = sb->data[at + i];
+    }
+    result[count] = '\0';
+    return result;
+}
+
+#endif //SB_IMPLEMENTATION
 #endif //SB_H
 
 /*
